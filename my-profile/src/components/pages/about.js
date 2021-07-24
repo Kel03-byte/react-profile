48bee58d63615814300ccbe002af15f16bae6c0c
Kellie Ryan <littlekel03@gmail.com>
2021-07-24 10:25:33 +0800

    Updated About Me section

--- my-profile/src/components/pages/about.js
+++ my-profile/src/components/pages/about.js
@@ -7,15 +7,14 @@ export default function About() {
         <div>
             <h1 id="header1">About Me</h1>
             <img id="headshot" src={Image} alt="Headshot of a lady"></img>
-            <div id="about-me">
-                <p>
+            <div id="about">
+                <p id="about-text">
                     Hi I am a current UWA Coding Bootcamp student and amd enjoying
                     the tough challenges that I have been doing so far. I am also a
                     full time worker at the Airport for a car rental company. I have
                     always been interested in tech and gaming and finally have the
                     opportunity to learn more about the process behind some of my favourite
-                    games and tech. The links below are to
-                    some of my work at UWA.
+                    games and tech.
                 </p>
             </div>
         </div>
