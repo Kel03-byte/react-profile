d3b489270a516eaf127169a1082fc03ef13cc65a
Kellie Ryan <littlekel03@gmail.com>
2021-07-25 09:46:55 +0800

    Updated portfolio page style

--- my-profile/src/components/pages/project.js
+++ my-profile/src/components/pages/project.js
@@ -4,17 +4,20 @@ import projects from '../../projects';
 export default function Project() {
   return (
     <div id="header2">
-      <h1>Work</h1>
+      <h1>Portfolio</h1>
       <ul id="project">
         {projects.map((project) => (
-          <li id="card" key={project.title}>
+          <li className="card" key={project.title}>
             <div id="card-title">{project.title}</div>
-            <div id="card-body">
+            <img id="card-image" src={project.image} alt={project.alt}></img>
+            <div className="card-text">
               <p>{project.summary}</p>
               <p>{project.tech}</p>
-              <p ><a id="nav-text" href={`${project.gitHub}`}>GitHub</a></p>
-              <p ><a id="nav-text" href={`${project.website}`}>{project.title}</a></p>
-              <img id="card-image" src={project.image} alt={project.alt}></img></div>
+              <p>
+              <a id="card-icon" href={`${project.website}`}><i className="fas fa-link fa-2x"></i></a>
+              <a id="card-icon" href={`${project.gitHub}`}><i className="fab fa-github fa-2x"></i></a>
+            </p>
+            </div>
           </li>
 
         ))}
